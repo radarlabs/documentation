c116fd9360d2eb709f2a3d9497a19cfe25e6041c
Jeff Kao <jeff.kao.000@gmail.com>
2021-08-23 14:44:55 -0700

    Auto-generate categories

--- scripts/regenerate-templated-docs.js
+++ scripts/regenerate-templated-docs.js
@@ -1,6 +1,8 @@
 const ejs = require('ejs');
-const chains = require('../schemas/chains/chains.json')
 const fs = require('fs');
+
+const chains = require('../schemas/chains/chains.json')
+const categories = require('../schemas/categories/categories.json')
 
 const generateChains = async () => {
   const template ="./templates/places/_chains_table.mdx.ejs";
@@ -8,16 +10,57 @@ const generateChains = async () => {
 
   console.log(`Regenerating ${output} from ${template}`);
 
-  const chainsTable =
+  const table =
     await ejs.renderFile(
       template,
       { chains },
       { async: true, rmWhitespace: true },
     );
 
-  fs.writeFileSync(output, chainsTable)
+  fs.writeFileSync(output, table)
 };
 
-Promise.all([
-  generateChains(),
-]);
+const generateTopLevelCategories = async () => {
+  const template ="./templates/places/_top_level_categories.mdx.ejs";
+  const output = "./docs/places/_top_level_categories.mdx";
+
+  const topLevelWithDupes =
+    Object.keys(categories).map((slug) => {
+      return categories[slug].categories[0];
+    });
+
+  const topLevelCategories = [...new Set(topLevelWithDupes)].sort();
+
+  console.log(`Regenerating ${output} from ${template}`);
+
+  const list =
+    await ejs.renderFile(
+      template,
+      { topLevelCategories },
+      { async: true, rmWhitespace: true },
+    );
+
+  fs.writeFileSync(output, list)
+};
+
+const generateCategories = async () => {
+  const template ="./templates/places/_categories_table.mdx.ejs";
+  const output = "./docs/places/_categories_table.mdx";
+
+  console.log(`Regenerating ${output} from ${template}`);
+
+  const table =
+    await ejs.renderFile(
+      template,
+      { categories },
+      { async: true, rmWhitespace: true },
+    );
+
+  fs.writeFileSync(output, table)
+};
+
+(async () => {
+  await generateChains();
+  await generateTopLevelCategories();
+  await generateCategories();
+})();
