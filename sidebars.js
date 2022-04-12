9870a0f12ac71cce352a6cb46d972420db77bacf
Brett Guenther <brett@radar.io>
2022-04-12 11:27:12 -0400

    initial how it works dev guide

--- sidebars.js
+++ sidebars.js
@@ -12,6 +12,7 @@
 module.exports = {
   defaultSidebar: [
     { type: "doc", id: "overview", label: "Overview" },
+    { type: "doc", id: "developer-guide", label: "Developer Guide" },
     "geofences",
     {
       type: "category",
@@ -95,6 +96,7 @@ module.exports = {
   ],
   miscSidebar: [
     { type: "doc", id: "overview", label: "Overview" },
+    { type: "doc", id: "developer-guide", label: "Developer Guide" },
     "geofences",
     {
       type: "category",
