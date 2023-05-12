cf88a16098336776c06f8188f40328160702070e
Craig Kochis <cjkochis@gmail.com>
2023-05-12 15:59:33 -0400

    use default map style

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,7 +8,7 @@ class Map extends React.Component {
   componentDidMount() {
     const map = new maplibregl.Map({
       container: CONTAINER,
-      style: `https://api.radar.io/maps/styles/radar-streets-beta?publishableKey=${PUBLISHABLE_KEY}`,
+      style: `https://api.radar.io/maps/styles/radar-default-v1?publishableKey=${PUBLISHABLE_KEY}`,
       center: [-73.9911, 40.7342], // NYC
       zoom: 11
     });
