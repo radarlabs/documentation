8bcc8701ab6594d41d42a11eec080b486ded7f71
Nick Patrick <nicholasjohnpatrick@gmail.com>
2023-05-16 22:13:02 -0400

    tweaks

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import maplibregl from 'maplibre-gl';
 
 const PUBLISHABLE_KEY = 'org_test_pk_7e1e22718af02cb9ed4a01233556999c2c02c947'; // same key as /demo/api
-const CONTAINER = 'map';
 
 class Map extends React.Component {
   componentDidMount() {
     const map = new maplibregl.Map({
-      container: CONTAINER,
+      container: 'map',
       style: `https://api.radar.io/maps/styles/radar-default-v1?publishableKey=${PUBLISHABLE_KEY}`,
       center: [-73.9911, 40.7342], // NYC
       zoom: 11
