f01373484ed7e32669f615e10f586c5d2beda956
Craig Kochis <cjkochis@gmail.com>
2023-07-20 15:22:35 -0400

    new tip icon

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,6 +4,10 @@ const childProcess = require('child_process');
 module.exports = {
   '*.mdx': [
     (filenames) => {
+      if (filenames.length === 0) {
+        return [];
+      }
+
       // check print spell-check output to console, dont block commit
       console.log('Spell-checking files:', filenames);
       childProcess.execSync(`cspell -c cspell.json ${filenames.join(' ')}`, { stdio: 'inherit' });
