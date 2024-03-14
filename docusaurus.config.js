6f79ebaeddc9d8c88fc38b6dd76a5689340ce489
Greg Sadetsky <lepetitg@gmail.com>
2024-03-14 18:40:16 -0400

    inject version number -- done for maps/maps page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,6 +71,34 @@ module.exports = {
       anonymizeIP: true,
     },
   },
+  plugins: [
+    // it should be possible to have this plugin
+    // live under src/plugins, but I (Greg) wasn't able to
+    // do that -- this is maybe related to our version of
+    // docusaurus. declaring the plugin "inline" below works.
+    () => ({
+        // name is crucial here for CodeBlock/index.js to get this plugin data
+        name: 'radar-version-injector',
+        async contentLoaded({content, actions}) {
+            const {setGlobalData} = actions;
+            const RADAR_SDK_REPOS = {
+                'web': 'radarlabs/radar-sdk-js',
+                'ios': 'radarlabs/radar-sdk-ios',
+                'android': 'radarlabs/radar-sdk-android',
+            }
+
+            // fetch latest release version number from github
+            const radarGitHubReleases = {};
+            for (const [sdkName, userRepo] of Object.entries(RADAR_SDK_REPOS)) {
+                const response = await fetch(`https://api.github.com/repos/${userRepo}/releases/latest`);
+                const data = await response.json();
+                radarGitHubReleases[`RADAR_${sdkName.toUpperCase()}_SDK_VERSION`] = data.name;
+            }
+
+            setGlobalData({radarGitHubReleases});
+        },
+    }),
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
