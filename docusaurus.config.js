740bfbb72776004bf2b3e705a487d9a7ac1264b7
Greg Sadetsky <lepetitg@gmail.com>
2024-03-19 08:16:18 -0400

    fetch sdk versions with delays to avoid rate limiting; cache them locally

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,22 +79,73 @@ module.exports = {
     // do that -- this is maybe related to our version of
     // docusaurus. declaring the plugin "inline" below works.
     () => ({
-        // name is crucial here for CodeBlock/index.js to get this plugin data
+        // `name` is crucial here for CodeBlock/index.js to get this plugin data
         name: 'radar-version-injector',
-        async contentLoaded({content, actions}) {
-            const {setGlobalData} = actions;
+        async contentLoaded({ actions }) {
+            const { setGlobalData } = actions;
             const RADAR_SDK_REPOS = {
-                'web': 'radarlabs/radar-sdk-js',
-                'ios': 'radarlabs/radar-sdk-ios',
-                'android': 'radarlabs/radar-sdk-android',
+                'web': 'radar-sdk-js',
+                'ios': 'radar-sdk-ios',
+                'android': 'radar-sdk-android',
+                'reactnative': 'react-native-radar',
+                'cordova': 'cordova-plugin-radar',
+                'capacitor': 'capacitor-radar',
+                'flutter': 'flutter-radar',
+                'xamarin': 'radar-sdk-xamarin'
             }
 
-            // fetch latest release version number from github
-            const radarGitHubReleases = {};
-            for (const [sdkName, userRepo] of Object.entries(RADAR_SDK_REPOS)) {
-                const response = await fetch(`https://api.github.com/repos/${userRepo}/releases/latest`);
-                const data = await response.json();
-                radarGitHubReleases[`RADAR_${sdkName.toUpperCase()}_SDK_VERSION`] = data.name;
+            // fetch latest release version number from github.
+            // there are two issues at play while doing this:
+            // fetching all of the sdk versions one after another from github
+            // will hit github's rate limit. we purposely delay each fetch below.
+            // the second issue is that while developing, docusaurus will reload
+            // this plugin continuously - that will also lead to rate limiting issues.
+            // use a local cache file to avoid this.
+
+            const fs = require('fs');
+            PATH_TO_CACHE_FILE = 'radar-github-releases.json';
+
+            let radarGitHubReleases = null;
+            let foundCache = false;
+
+            if (fs.existsSync(PATH_TO_CACHE_FILE)) {
+                const cachedGithubReleases = JSON.parse(fs.readFileSync(PATH_TO_CACHE_FILE));
+                // check timestamp, if it's older than 24 hours, then ignore cached versions
+                if (Date.now() - cachedGithubReleases.timestamp < 24 * 60 * 60 * 1000) {
+                    radarGitHubReleases = cachedGithubReleases.data;
+                    foundCache = true;
+                }
+            }
+
+            // we haven't found a cache, so fetch everything from github
+            if (!radarGitHubReleases) {
+                console.log(`Fetching versions from GitHub, this should take less than 30 seconds...`);
+
+                radarGitHubReleases = {};
+                for (const [sdkName, repo] of Object.entries(RADAR_SDK_REPOS)) {
+                    const [response] = await Promise.all([
+                        fetch(`https://api.github.com/repos/radarlabs/${repo}/releases/latest`),
+                        // delay between each api call to avoid rate limiting
+                        new Promise(resolve => setTimeout(resolve, 1000))
+                    ]);
+                    // check that response status code is 200, otherwise throw / stop immediately
+                    if (response.status !== 200) {
+                        throw new Error(`An error occurred while fetching versions from GitHub! ${response.status} -- ${response.statusText}`);
+                    }
+                    const data = await response.json();
+                    radarGitHubReleases[`RADAR_${sdkName.toUpperCase()}_SDK_VERSION`] = data.name;
+                }
+            }
+
+            // only write cache if we hadn't found one originally,
+            // otherwise we'll keep incorrectly updating the timestamp
+            if(!foundCache) {
+                fs.writeFileSync(PATH_TO_CACHE_FILE, JSON.stringify({data: radarGitHubReleases, timestamp: Date.now()}));
+            }
+
+            // sanity check -- check that web sdk version looks reasonable and isn't undefined
+            if (!radarGitHubReleases.RADAR_WEB_SDK_VERSION) {
+                throw new Error('Web SDK version is undefined! Something went wrong while fetching versions from GitHub.');
             }
 
             setGlobalData({radarGitHubReleases});
