53b679155b3e80444eb234bcb0c20c2609fb9969
Craig Kochis <cjkochis@gmail.com>
2025-03-27 12:37:21 -0400

    update version and examples to use Radar SDK

--- src/components/examples/GlobeMap.jsx
+++ src/components/examples/GlobeMap.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import Radar from 'radar-sdk-js';
-import maplibregl from "maplibre-gl";
 
 import 'radar-sdk-js/dist/radar.css';
 
@@ -10,14 +9,14 @@ const GlobeMap = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    if (mapRef.current) return; 
+    if (mapRef.current) return;
 
     Radar.initialize(PUBLISHABLE_KEY);
 
-    mapRef.current = new maplibregl.Map({
+    mapRef.current = Radar.ui.map({
       container: 'globe-map',
-      style: 'https://api.radar.io/maps/styles/radar-default-v1?&publishableKey=org_test_pk_7e1e22718af02cb9ed4a01233556999c2c02c947',
-      zoom: 1,
+      zoom: 2,
+      minZoom: 0,
       center: [-73.9910078, 40.7342465],
     });
 
@@ -34,7 +33,7 @@ const GlobeMap = () => {
   }, []);
 
   return (
-    <div style={{ height: '400px', margin: '1rem 0 1.5rem', position: 'relative', width: '100%' }}>
+    <div style={{ height: '600px', margin: '1rem 0 1.5rem', position: 'relative', width: '100%' }}>
       <div id="globe-map" style={{ bottom: 0, position: 'absolute', top: 0, width: '100%' }} />
     </div>
   );
